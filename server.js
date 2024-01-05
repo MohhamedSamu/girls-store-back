2946cca26cdf068844c6f8e7e8c84d60fddd8b0a
Marco Aguirre <aguirreg.marco@gmail.com>
2024-01-04 20:41:22 -0600

    added body parser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+var bodyParser = require('body-parser')
 
 app.use(bodyParser.json({ limit: "50mb" }))
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
